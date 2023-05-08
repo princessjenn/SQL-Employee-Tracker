4e219270e27cf78db2611e7ff734672decbb0c72
Jennifer Eckenrode <j.eckenrode@me.com>
2023-05-07 22:13:24 -0500

    corrected tables to function in SQL workbench properly

--- schema.sql
+++ schema.sql
@@ -1,28 +1,29 @@
-DROP DATABASE employees_db IF EXISTS;
+DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name: VARCHAR(30)
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title: VARCHAR(30),
-    salary: DECIMAL, 
-    department_id: INT
+    title VARCHAR(30),
+    salary DECIMAL, 
+    department_id INT
 
 );
 
 CREATE TABLE employee (
-    id: INT AUTO_INCREMENT PRIMARY KEY,
-    first_name: VARCHAR(30),
-    last_name: VARCHAR(30),
-    role_id: INT, 
-    manager_id: INT
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT, 
+    manager_id INT
 );
+
 
 
 * department
