5c07624b14623ab15bf2f4a99cb76b7c00f87b35
Jennifer Eckenrode <j.eckenrode@me.com>
2023-05-08 23:50:13 -0500

    added try/catch statments to async functions

--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,28 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT AUTO_INCREMENT ,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
-    salary DECIMAL(10,2),
+    salary DECIMAL,
     department_id INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
+    PRIMARY KEY (id)
     FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE
+    SET NULL
 );
-
---  I added foreign key constraints to the role and employee tables to reference the department AND employee tables. I also specified a precision and scale for the salary column in the role table.
-
---The precision and scale are used to specify the maximum range and precision of a decimal number, which affects the amount of storage space required to store the number. It is important to choose an appropriate precision and scale for a column in a table to avoid truncation or rounding errors when performing calculations or storing values.
--- index.mjs
+++ index.mjs
@@ -1,63 +1,47 @@
 
-const mysql = require('mysql2/promise');
-const inquirer = require('inquirer');
+import inquirer from 'inquirer';
+import { viewAllEmployees, addEmployee, updateEmployeeRole } from './lib/employee.js';
+import { viewAllDepartments, addDepartment } from './lib/department.js';
+import { viewAllRoles, addRole } from './lib/role.js';
 
-const { viewAllEmployees, addEmployee, updateEmployeeRole } = require('./lib/employee');
-const { viewAllDepartments, addDepartment } = require('./lib/department');
-const { viewAllRoles, addRole } = require('./lib/role');
+const userActions = {
+  'View all departments': () => viewAllDepartments(),
+  'View all roles': () => viewAllRoles(),
+  'View all employees': () => viewAllEmployees(),
+  'Add a department': () => addDepartment(),
+  'Add a role': () => addRole(),
+  'Add an employee': () => addEmployee(),
+  'Update an employee role': () => updateEmployeeRole(),
+  'Quit': () => quit
+};
 
-const pool = mysql.createPool(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'rootroot',
-        database: 'employees_db',
-        waitForConnections: true,
-        connectionLimit: 10,
-        maxIdle: 10, //max idle connections, the default val is the same as 'connectionLimit'
-        idleTimeout: 60000, //idle connections timeout, in milliseconds, the default value 60000
-        queueLimit: 0
+async function start() {
+  //function to await on user input:
+  const answer = await inquirer.prompt({
+    name: 'action',
+    type: 'list',
+    message: 'Welcome to your Employee Database Tracker. What would you like to view?',
+    choices: [...Object.keys(userActions), 'Quit'],
+  });
 
-      });
+  if (answer.action === 'Quit') { // Check if user chose to exit
+    return; // Return from function to exit program
+  }
+  //calling the function based on the user's input:
+  await userActions[answer.action]();
 
-      const userActions = {
-        'View all departments': () => viewAllDepartments,
-        'View all roles': () => viewAllRoles,
-        'View all employees': () => viewAllEmployees,
-        'Add a department': () => addDepartment,
-        'Add a role': () => addRole,
-        'Add an employee': () => addEmployee,
-        'Update an employee role': () => updateEmployeeRole,
-        'Quit': () => quit
-      };
+  start();
+}
 
-      async function start() {
-        //function to await on user input:
-        const answer = await inquirer.prompt({
-          name: 'action',
-          type: 'list',
-          message: 'Welcome to your Employee Database Tracker. What would you like to view?',
-          choices: [...Object.keys(userActions) , 'Quit'],
-        });
-
-        if (answer.action === 'Quit') { // Check if user chose to exit
-          return; // Return from function to exit program
-        }  
-      //calling the function based on the user's input:
-      await userActions[answer.action]();
-
-       start(); 
-    }
-
-    async function quit() {
-      console.log('Exiting program...Goodbye!');
-      process.exit(0); // Exit the program with a status code of 0 (success)
-    }
+async function quit() {
+  console.log('Exiting program...Goodbye!');
+  process.exit(0); // Exit the program with a status code of 0 (success)
+}
 
 //export modules
-module.exports = { start }
+module.exports = { start, quit };
 
 
-start ();
+start();
 
 
